e17ad9f27235855e17a5b245956c7a87d6af0d41
simonhmadsen <60658195+simonhmadsen@users.noreply.github.com>
2022-02-04 07:03:08 +0100

    Update Env.elm

--- src/Env.elm
+++ src/Env.elm
@@ -3,6 +3,10 @@ module Env exposing (..)
 -- The Env.elm file is for per-environment configuration.
 -- See https://dashboard.lamdera.app/docs/environment for more info.
 
+-- Edit this file to contain the Auth0 ClientID and Host URL of your Auth0 Application.
+-- Make sure to mark at least the clientId as Backend Only in your Lamdera configuration 
+-- (https://dashboard.lamdera.app/docs/environment#secret-and-public-config-values)
+
 
 clientId =
     ""
